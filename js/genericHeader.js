b3255299ed6afae50e272a2248f7b07284bffa85
Mart-Dan-Rossi <94561321+Mart-Dan-Rossi@users.noreply.github.com>
2022-09-24 15:26:17 -0300

    Finalizo primer diseño de navbar responsive

--- js/genericHeader.js
+++ js/genericHeader.js
@@ -2,10 +2,15 @@ let etiquetaHeader = document.querySelector("header")
 let contenidoHeader = ``
 
 let botonDropdown = document.querySelector(".productos-con-icono-dropdown")
-let contenidoDesplegable = document.querySelector(".navbar .productos .dropbtn")
+let contenidoDesplegable = document.querySelector(".navbar .productos")
 
-function controladorDropdown(contenidoADesplegar) {  
+function toggleHiddenAlElemento(contenidoADesplegar) {  
   return ()=>{contenidoADesplegar.classList.toggle("hidden")}
 }
 
-botonDropdown.addEventListener("click", controladorDropdown(contenidoDesplegable))
+botonDropdown.addEventListener("click", toggleHiddenAlElemento(contenidoDesplegable))
+
+let navbar = document.querySelector(".navbar")
+let iconoMenuHamburguesa = document.querySelector(".icono-hamburguesa")
+
+iconoMenuHamburguesa.addEventListener("click", toggleHiddenAlElemento(navbar))
