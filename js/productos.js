6f2caf21c65dd1628a73f2874a5f894aca1b1086
Gui1e <gep2@protonmail.com>
2022-10-06 22:06:54 -0300

    Agrego lista de productos y algunos cambios de estilo

---
+++ js/productos.js
@@ -0,0 +1,64 @@
+[{ 
+    imagen: "/recursos/Productos/Productos residenciales/inversor solar hibrido 2.4kw.png",
+    nombre: "Inversor solar híbrido PV1100",
+    precio: "$127000",
+    descripcion:"Permite la posibilidad de combinar energía solar, energía de red y energía de batería para alimentar los consumos de manera ininterrumpida.",
+},{ 
+    imagen: "/recursos/Productos/Productos residenciales/Kit Inversor solar y batería 1a2KW.png",
+    nombre: "Sistema de almacenamiento de energía solar (inversor + batería) HBP3000",
+    precio: "$63722",
+    descripcion:"Sistema versátil (Inversor + Batería Solar de Litio) de almacenamiento de energía para su hogar, de 2KW.",
+
+},{
+    imagen: "/recursos/Productos/Productos residenciales/bateria solar de litio 1-3kw.png",
+    nombre: "Batería de Litio LP20",
+    precio: "$140640",
+    descripcion:"Batería de litio para el hogar, compacta y transportable (51.2V/20Ah)",
+},{ 
+    imagen: "/recursos/Productos/Productos residenciales/panel solar 150w.png",
+    nombre: "Panel Mono Solar 150W",
+    precio: "$25361",
+    descripcion:"Panel mono solar de 150W, alta eficiencia y calidad, con 36 células ",
+},{ 
+    imagen: "/recursos/Productos/Productos residenciales/panel solar 250w 60 celdas.png",
+    nombre: "Panel Solar 250W",
+    precio: "$30200",
+    descripcion:"Panel solar de 250W, alta eficiencia y calidad, con 60 células",
+},{ 
+    imagen: "/recursos/Productos/Productos comerciales/Inversor Solar con Baterías de Litio Serie HBP1800 PRO (5.2KW).png",
+    nombre: "Inversor Solar con Sistema de Almacenamiento con Baterías de Litio Serie HBP1800 (5.2KW)",
+    precio: "$570360",
+    descripcion:"Sistema de almacenamiento de energía con baterías de litio(3U) modular, precableado, con inversor solar integrado, de 5.2KW.",
+},{ 
+    imagen: "/recursos/Productos/Productos comerciales/panel solar 72 celdas 350W.png",
+    nombre: "Panel Solar 350W",
+    precio: "$50900",
+    descripcion:"Panel solar de 350W, alta eficiencia y calidad , con 72 células",
+},{ 
+    imagen: "/recursos/Productos/Productos comerciales/panel solar bifacial 535w 144 celdas.png",
+    nombre: "Panel Solar Bifacial de 535W ",
+    precio: "$77804",
+    descripcion:"Panel solar bifacial de 535W, alta eficiencia y calidad, con 144 células.",
+},{ 
+    imagen: "/recursos/Productos/Productos comerciales/producto comercial 1 Bateria de litio+inversor 3kw.png",
+    nombre: "Sistema solar integrado P3400 (3KW)",
+    precio: "$270350",
+    descripcion:"Sistema solar integrado todo en uno (Baterías de litio + Inversor solar de 3000W) de 3KW",
+},{ 
+    imagen: "/recursos/Productos/Productos comerciales/Sistema de almacenamiento modular de energía solar 5.2KW(Batería + Inversor).png",
+    nombre: "Sistema de Almacenamiento Solar Modular",
+    precio: "$353020",
+    descripcion:"Sistema de almacenamiento solar modular, precableado de 5.2KW, fácil de actualizar, ampliar y mantener.",
+},{ 
+    imagen: "/recursos/Productos/Productos comerciales/sistema solar bateria + inversor 6.4kw.png",
+    nombre: "Sistema solar integrado P6400 (6.4KW)",
+    precio: "$535100",
+    descripcion:"Sistema solar versátil integrado, todo en uno (Baterías de litio + Inversor solar de 6000W) de 6.4KW",
+},{ 
+    imagen: "",
+    nombre: "",
+    precio: "",
+    descripcion:"",
+},
+
+]
